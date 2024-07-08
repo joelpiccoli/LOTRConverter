c354ff6d8478cb9973b3ec48129f6f1a9e52faf5
Joel Piccoli da Rosa <joel.picc@outlook.com>
2024-07-08 14:28:09 -0300

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Joel da Rosa on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
